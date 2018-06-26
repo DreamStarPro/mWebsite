0a201e244e85efd65fcc4993cafd8933beaca1ca
G. S. Varma <17321286+gsvarma@users.noreply.github.com>
2018-06-26 12:04:54 +0200

    Add Nav

--- JavaScript/quoteGenerator.js
+++ JavaScript/quoteGenerator.js
@@ -1,4 +1,4 @@
-var quotes = [
+const quotes = [
     'Don\'t Worry, Be Happy!',
     'No amount of prayer or meditation can do what helping others can do.',
     'I love everybody. Each one plays the role they have to play...',
@@ -9,9 +9,9 @@ var quotes = [
     'True love is no game of the faint-hearted and the weak. It is born of strength and understanding.',
     'The human form was never [formed] to beget children. This tendency among people to cohabit is nothing but ' +
     'animal instinct inherited from all the previous lives of evolution from the stone to the human form.'
-]
+];
 
 function newQuote() {
-    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    const randomNumber = Math.floor(Math.random() * (quotes.length));
     document.getElementById('showQuote').innerHTML = quotes[randomNumber];
 }
