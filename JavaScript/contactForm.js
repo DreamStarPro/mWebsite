16894754149e20b95c84688e511978f0a8d46b9c
G. S. Varma <17321286+gsvarma@users.noreply.github.com>
2018-07-08 23:34:11 +0200

    Add firebase

---
+++ JavaScript/contactForm.js
@@ -0,0 +1,59 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCZWJJmz2uRU0a1QZf3g9Bc1hdG-6a7scM",
+    authDomain: "mwebsite-dsp.firebaseapp.com",
+    databaseURL: "https://mwebsite-dsp.firebaseio.com",
+    projectId: "mwebsite-dsp",
+    storageBucket: "mwebsite-dsp.appspot.com",
+    messagingSenderId: "131867965601"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('contactUS_messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    // Get values
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(name, email, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function () {
+        document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+
+    // Clear form
+    document.getElementById('contactForm').reset();
+
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        message: message
+    });
+}
